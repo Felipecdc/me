f3e2989e9a33abc6c383aee66ba931ee2024fd2e
Felipe Castro <felipecdc09@gmail.com>
2024-06-26 20:09:57 -0300

    feat: update home page dor create header component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../_components/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>teste</h1>
+      </div>
+    </>
+  );
+}
