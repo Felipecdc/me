bd0583cfea3c0a13b07dd917ff1db493ff0a438c
Felipe Castro <felipecdc09@gmail.com>
2024-07-04 19:25:25 -0300

    feat: change width on footer field

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
       <CustomCard>
         <div className="mt-0 flex flex-col gap-2">
           <h1 className="text-2xl font-bold">Vamos criar algo juntos?</h1>
-          <p className="max-w-[540px]">
-            Se você quiser elevar o nivel e valor da sua marca ou empresa,
+          <p className="max-w-[560px]">
+            Se você quiser elevar o nivel e o valor da sua marca ou empresa,
             sinta-se à vontade para me enviar uma mensagem.
           </p>
           <Button className="mt-5 h-8 rounded-2xl md:w-32">Fale comigo</Button>
@@ -17,7 +17,7 @@ const Footer = () => {
       <CustomCard>
         <h1 className="line-clamp-1 text-[#616161]">
           <strong>Criado com: </strong>
-          Typescript, React, NextJs, PostgreSQL, Prisma, Shadcn-ui
+          Typescript, React, NextJs, PostgreSQL, Prisma, Shadcn-ui...
         </h1>
       </CustomCard>
     </>
