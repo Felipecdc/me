db15cfe23d9aab72ec5ba09240f8e93539a0859e
Felipe Castro <felipecdc09@gmail.com>
2024-07-06 19:08:36 -0300

    feat: add font

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import CustomCard from "./custom-card";
 import { Button } from "./ui/button";
 
@@ -6,18 +7,23 @@ const Footer = () => {
     <>
       <CustomCard>
         <div className="mt-0 flex flex-col gap-2">
-          <h1 className="text-2xl font-bold">Vamos criar algo juntos?</h1>
-          <p className="max-w-[560px]">
+          <h1 className="font-cairo text-2xl font-bold">
+            Vamos criar algo juntos?
+          </h1>
+          <p className="font-cairo max-w-[560px]">
             Se você quiser elevar o nivel e o valor da sua marca ou empresa,
             sinta-se à vontade para me enviar uma mensagem.
           </p>
-          <Button className="mt-5 h-8 rounded-2xl shadow-md shadow-[#242424] md:w-32">
-            Fale comigo
+          <Button
+            className="mt-5 h-8 rounded-2xl shadow-md shadow-[#242424] md:w-32"
+            asChild
+          >
+            <Link href={"/about"}>Fale comigo</Link>
           </Button>
         </div>
       </CustomCard>
       <CustomCard>
-        <h1 className="line-clamp-1 text-[#616161]">
+        <h1 className="font-cairo line-clamp-1 text-[#616161]">
           <strong>Criado com: </strong>
           Typescript, React, NextJs, PostgreSQL, Prisma, Shadcn-ui...
         </h1>
