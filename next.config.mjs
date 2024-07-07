799aeca6d360bd0feccf134e8b272b703bf2e4b4
Felipe Castro <felipecdc09@gmail.com>
2024-07-07 18:57:46 -0300

    fix: config next for use images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "felipe-portifolio.s3.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
