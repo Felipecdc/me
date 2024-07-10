feac8718a89a3b37cdf1a843f249c7c53a5f5c78
Felipe Castro <felipecdc09@gmail.com>
2024-07-10 18:14:40 -0300

    feat: create designs file

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -1,11 +1,59 @@
+import Image from "next/image";
 import Footer from "../_components/footer";
 import Header from "../_components/header";
+import CustomCard from "../_components/custom-card";
 
 const Design = () => {
   return (
     <>
       <Header />
-      <div className="pt-20"></div>
+      <div className="flex items-center justify-center px-5 pt-14 md:pt-20">
+        <div className="mb-5 mt-8 flex w-full max-w-[1100px] flex-col items-start justify-center gap-1">
+          <div className="flex">
+            <div className="flex w-full max-w-[550px] flex-col gap-4">
+              <h3 className="font-cairo text-2xl font-bold md:text-3xl">
+                Designs publicos
+              </h3>
+              <p className="font-cairo text-base">
+                Criei está pagina com o intuito de publicar designs prontos para
+                você que não tem ideias sobre UI, mas que esta querendo fazer
+                seus projetos frontend.
+              </p>
+              <p className="font-cairo text-base">
+                Aqui vocé encontrará designs que poderão ser utilizados em
+                aplicações web e mobille, e tambem aproveite e explore a galeria
+                para assim pegar ideias e poder criar os seus proprios.
+              </p>
+              <h3 className="font-cairo text-xl">
+                Obs: Todos os layouts estão dispostos no{" "}
+                <a
+                  className="text-purple-400"
+                  href="https://figma.com/"
+                  target="_blank"
+                >
+                  Figma
+                </a>
+              </h3>
+            </div>
+            {/* <div className="hide-on-small flex h-auto w-[550px] items-center justify-end pr-20">
+              <div className="relative h-[310px] w-[300px]">
+                <Image
+                  src="/laptop.png"
+                  alt="Felipe Castro"
+                  style={{ objectFit: "cover" }}
+                  className="rounded-md"
+                  fill
+                />
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center px-5 pt-6">
+        <CustomCard>
+          <h1>Em construção!</h1>
+        </CustomCard>
+      </div>{" "}
       <div className="flex flex-col items-center justify-center gap-3 px-5 py-6">
         <Footer />
       </div>
