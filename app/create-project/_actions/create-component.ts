b4ee45eff70a9f4ce59ff8488907f68b01dc0f81
Felipe Castro <felipecdc09@gmail.com>
2024-07-29 19:30:21 -0300

    chore: create component and action for create code components

---
+++ app/create-project/_actions/create-component.ts
@@ -0,0 +1,90 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { v4 } from "uuid";
+import { z } from "zod";
+
+const formSchema = z.object({
+  name: z.string().min(2, "Nome muito curto").max(50, "Nome muito longo"),
+  imageUrl: z
+    .instanceof(File)
+    .refine((file) => file instanceof File, "Você deve selecionar uma imagem"),
+  codeDependencies: z.string().min(8, "Codigo muito curto"),
+  codeMain: z.string().min(8, "Codigo muito curto"),
+  codeImport: z.string().min(8, "Codigo muito curto"),
+  code: z.string().min(8, "Codigo muito curto"),
+  description: z.string().optional(),
+});
+
+export const createComponent = async (form: FormData) => {
+  const validation = formSchema.safeParse({
+    name: form.get("name"),
+    imageUrl: form.get("imageUrl") as File,
+    codeDependencies: form.get("codeDependencies"),
+    codeMain: form.get("codeMain"),
+    codeImport: form.get("codeImport"),
+    code: form.get("code"),
+    description: form.get("description"),
+  });
+
+  if (validation.success) {
+    const {
+      name,
+      imageUrl,
+      codeDependencies,
+      codeMain,
+      codeImport,
+      code,
+      description,
+    } = validation.data;
+
+    const component = await db.components.create({
+      data: {
+        id: v4(),
+        name,
+        imageUrl: "",
+        codeDependencies,
+        codeMain,
+        codeImport,
+        code,
+        description,
+      },
+    });
+
+    const buffer = (await imageUrl.arrayBuffer()) as Buffer;
+
+    const client = new S3Client({
+      region: process.env.AWS_RIGION,
+    });
+
+    const key = `components-picture/${v4()}.${imageUrl.type.split("/")[1]}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      ACL: "public-read",
+      Key: key,
+      Body: buffer,
+    });
+
+    await db.components.update({
+      where: {
+        id: component.id,
+      },
+      data: {
+        imageUrl: `https://${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${key}`,
+      },
+    });
+
+    try {
+      await client.send(command);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log(validation.error);
+    return {
+      errors: validation.error.issues,
+    };
+  }
+};
---
+++ app/create-project/_components/components-form.tsx
@@ -0,0 +1,221 @@
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../_components/ui/form";
+import { Input } from "../../_components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import SubmitButton from "./submit-button";
+import { createComponent } from "../_actions/create-component";
+
+const formSchema = z.object({
+  name: z.string().min(2, "Nome muito curto").max(50, "Nome muito longo"),
+  imageUrl: z
+    .instanceof(File)
+    .refine((file) => file instanceof File, "Você deve selecionar uma imagem"),
+  codeDependencies: z.string().min(8, "Codigo muito curto"),
+  codeMain: z.string().min(8, "Codigo muito curto"),
+  codeImport: z.string().min(8, "Codigo muito curto"),
+  code: z.string().min(8, "Codigo muito curto"),
+  description: z.string().optional(),
+});
+
+type ComponentsFormData = z.infer<typeof formSchema>;
+
+const ComponentsForm = () => {
+  const form = useForm<ComponentsFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: undefined,
+      codeDependencies: "",
+      codeMain: "",
+      codeImport: "",
+      code: "",
+      description: "",
+    },
+  });
+
+  const onSubmit = async (data: ComponentsFormData) => {
+    try {
+      const formData = new FormData();
+      formData.append("name", data.name);
+      formData.append("imageUrl", data.imageUrl);
+      formData.append("codeImport", data.codeImport);
+      formData.append("code", data.code);
+      if (data.codeDependencies !== undefined)
+        formData.append("codeDependencies", data.codeDependencies);
+      if (data.codeMain !== undefined)
+        formData.append("codeMain", data.codeMain);
+      if (data.description !== undefined)
+        formData.append("description", data.description);
+
+      const result = await createComponent(formData);
+
+      if (result?.errors) {
+        result.errors.forEach((error) => {
+          form.setError(error.path[0] as keyof ComponentsFormData, {
+            type: "manual",
+            message: error.message,
+          });
+        });
+      } else {
+        alert("Componente criado");
+        form.reset();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          // action={createProject}
+          className="flex w-[350px] flex-col gap-5 rounded-lg bg-card p-5 md:w-[600px]"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel className="text-lg font-semibold">Name</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-input shadow-md shadow-stone-900"
+                    placeholder="Digite o nome do componente"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel className="text-lg font-semibold">Imagem</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-input shadow-md shadow-stone-900"
+                    placeholder="Selecione uma imagem"
+                    type="file"
+                    accept="image/png"
+                    onChange={(e) =>
+                      field.onChange(e.target.files ? e.target.files[0] : null)
+                    }
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="codeImport"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel className="text-lg font-semibold">Import</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-input shadow-md shadow-stone-900"
+                    placeholder="Código de Importação"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="code"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel className="text-lg font-semibold">Code</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-input shadow-md shadow-stone-900"
+                    placeholder="Código principal"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="codeDependencies"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel className="text-lg font-semibold">
+                  Dependencies
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-input shadow-md shadow-stone-900"
+                    placeholder="Dependências"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="codeMain"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel className="text-lg font-semibold">Main</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-input shadow-md shadow-stone-900"
+                    placeholder="Código arquivo main"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel className="text-lg font-semibold">
+                  Description
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-input shadow-md shadow-stone-900"
+                    placeholder="Descrição"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <SubmitButton
+            isSubmitting={form.formState.isSubmitting}
+            text="Criar Componente"
+          />
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default ComponentsForm;
