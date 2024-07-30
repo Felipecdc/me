739468e0850e8f2f11ea9c151b1a777a1b5993f6
Felipe Castro <felipecdc09@gmail.com>
2024-07-30 18:11:15 -0300

    feat: update security page for create data in database

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,47 +1,13 @@
 "use client";
 
-import { z } from "zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "../_components/ui/form";
-import { Input } from "../_components/ui/input";
-import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { SendHorizontalIcon } from "lucide-react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createProject } from "./_actions/create-project";
-import SubmitButton from "./_components/submit-button";
-
-const formSchema = z.object({
-  name: z.string().min(2, "Nome muito curto").max(50, "Nome muito longo"),
-  imageUrl: z
-    .instanceof(File)
-    .refine((file) => file instanceof File, "Você deve selecionar uma imagem"),
-  status: z.string().min(2, "Status muito curto").max(10, "Status muito longo"),
-  github: z.string().optional(),
-  linkedin: z.string().optional(),
-  deploy: z.string().optional(),
-});
-
-type ProjectFormData = z.infer<typeof formSchema>;
+import ProjectForm from "./_components/project-form";
+import ComponentsForm from "./_components/components-form";
+import DesignForm from "./_components/design-form";
 
 const CreateProject = () => {
-  const form = useForm<ProjectFormData>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      imageUrl: undefined,
-      status: "",
-      github: "",
-      linkedin: "",
-      deploy: "",
-    },
-  });
+  const [formSelected, setFormSelected] = useState("Projects");
 
   // Loading page
   const [keyScreen, setKeyScreen] = useState<boolean>(true);
@@ -78,160 +44,28 @@ const CreateProject = () => {
   }
   // Finalloading page
 
-  const onSubmit = async (data: ProjectFormData) => {
-    try {
-      const formData = new FormData();
-      formData.append("name", data.name);
-      formData.append("imageUrl", data.imageUrl);
-      formData.append("status", data.status);
-      if (data.github !== undefined) formData.append("github", data.github);
-      if (data.linkedin !== undefined)
-        formData.append("linkedin", data.linkedin);
-      if (data.deploy !== undefined) formData.append("deploy", data.deploy);
-
-      const result = await createProject(formData);
-
-      if (result?.errors) {
-        result.errors.forEach((error) => {
-          form.setError(error.path[0] as keyof ProjectFormData, {
-            type: "manual",
-            message: error.message,
-          });
-        });
-      } else {
-        alert("Projeto criado");
-        form.reset();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center pb-8 pt-5">
       <h1 className="mb-5 text-xl font-semibold uppercase">
-        Cadastrar um novo projeto
+        Cadastrar um novo
+        {formSelected === "Projects" && " projeto"}
+        {formSelected === "Components" && " componente"}
+        {formSelected === "Design" && " design"}
       </h1>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          // action={createProject}
-          className="flex w-[350px] flex-col gap-5 rounded-lg bg-card p-5 md:w-[600px]"
+      <div className="mb-5 flex h-8 w-[350px] items-center justify-center rounded-md bg-card px-5">
+        <select
+          className="h-full w-full bg-transparent text-card-foreground"
+          value={formSelected}
+          onChange={(e) => setFormSelected(e.target.value)}
         >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem className="space-y-1">
-                <FormLabel className="text-lg font-semibold">Name</FormLabel>
-                <FormControl>
-                  <Input
-                    className="bg-input shadow-md shadow-stone-900"
-                    placeholder="Digite o nome do projeto"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="imageUrl"
-            render={({ field }) => (
-              <FormItem className="space-y-1">
-                <FormLabel className="text-lg font-semibold">Imagem</FormLabel>
-                <FormControl>
-                  <Input
-                    className="bg-input shadow-md shadow-stone-900"
-                    placeholder="Descarrega uma imagem"
-                    type="file"
-                    accept="image/png"
-                    onChange={(e) =>
-                      field.onChange(e.target.files ? e.target.files[0] : null)
-                    }
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="status"
-            render={({ field }) => (
-              <FormItem className="space-y-1">
-                <FormLabel className="text-lg font-semibold">Status</FormLabel>
-                <FormControl>
-                  <Input
-                    className="bg-input shadow-md shadow-stone-900"
-                    placeholder="Status do projeto"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="github"
-            render={({ field }) => (
-              <FormItem className="space-y-1">
-                <FormLabel className="text-lg font-semibold">GitHub</FormLabel>
-                <FormControl>
-                  <Input
-                    className="bg-input shadow-md shadow-stone-900"
-                    placeholder="Digite a url do repositorio"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="linkedin"
-            render={({ field }) => (
-              <FormItem className="space-y-1">
-                <FormLabel className="text-lg font-semibold">
-                  LinkedIn
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    className="bg-input shadow-md shadow-stone-900"
-                    placeholder="Digite a url do post"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="deploy"
-            render={({ field }) => (
-              <FormItem className="space-y-1">
-                <FormLabel className="text-lg font-semibold">Deploy</FormLabel>
-                <FormControl>
-                  <Input
-                    className="bg-input shadow-md shadow-stone-900"
-                    placeholder="Digite a url do deploy"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <SubmitButton
-            isSubmitting={form.formState.isSubmitting}
-            text="Criar Projeto"
-          />
-        </form>
-      </Form>
+          <option value="Projects">Projects</option>
+          <option value="Components">Components</option>
+          <option value="Design">Design</option>
+        </select>
+      </div>
+      {formSelected === "Projects" && <ProjectForm />}
+      {formSelected === "Components" && <ComponentsForm />}
+      {formSelected === "Design" && <DesignForm />}
     </div>
   );
 };
