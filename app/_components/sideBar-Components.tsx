8fa0c17d374815dc4d0e7a545fa16bb71a7ff2fb
Felipe Castro <felipecdc09@gmail.com>
2024-08-01 17:34:00 -0300

    feat: update size in values on card

--- app/_components/sideBar-Components.tsx
+++ app/_components/sideBar-Components.tsx
@@ -5,7 +5,7 @@ import { SheetTitle } from "./ui/sheet";
 const SideBarComponents = (item: ComponentsProps) => {
   return (
     <>
-      <SheetTitle className="text-xl font-bold">{item.name}</SheetTitle>
+      <SheetTitle className="text-2xl font-bold">{item.name}</SheetTitle>
       <div className="pb-7 pt-3">
         <span className="text-sm text-card-foreground">
           {item.description ||
@@ -16,24 +16,24 @@ const SideBarComponents = (item: ComponentsProps) => {
         {item.codeDependencies && (
           <div>
             <h3 className="mb-1 text-lg font-semibold">Dependências</h3>
-            <CodeBlock code={item.codeDependencies} />
+            <CodeBlock code={item.codeDependencies} maxHeight="100" />
           </div>
         )}
         <div>
           <h3 className="mb-1 text-lg font-semibold">Importação</h3>
-          <CodeBlock code={item.codeImport} />
+          <CodeBlock code={item.codeImport} maxHeight="100" />
         </div>
         {item.codeMain && (
           <div>
             <h3 className="mb-1 text-lg font-semibold">
               Código do arquivo principal
             </h3>
-            <CodeBlock code={item.codeMain} />
+            <CodeBlock code={item.codeMain} maxHeight="220" />
           </div>
         )}
         <div>
           <h3 className="mb-1 text-lg font-semibold">Código</h3>
-          <CodeBlock code={item.code} />
+          <CodeBlock code={item.code} maxHeight="500" />
         </div>
       </div>
     </>
